0f6364523ca2f107d1f0c985a2610ea736b06f30
Jaeheon Shim <git@jaeheonshim.com>
2024-09-27 21:40:42 -0400

    MongoDB integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
+import { signIn } from "@/auth";
+import { SignIn } from "@/components/auth/SignInButton";
 import Image from "next/image";
-import { signIn } from "../auth";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-       <button onClick={async () => {
-        "use server"
-        await signIn();
-       }}>Sign In (Testing)</button>
+      <SignIn />
     </div>
   );
 }
